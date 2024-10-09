db1769eafa7bf61826ad00cc5072ec3da280bed0
@hexlet/cli <support@hexlet.io>
2024-10-09 16:40:53 +0300

    submit java-spring-ru/rest-api

---
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -0,0 +1,44 @@
+package exercise.controller.users;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import exercise.model.Post;
+import exercise.Data;
+
+// BEGIN
+@RestController
+@RequestMapping("/api")
+public class PostsController {
+
+    public static List<Post> posts = Data.getPosts();
+
+    @GetMapping("/users/{id}/posts")
+    public List<Post> index(@PathVariable Integer id) {
+        return posts.stream()
+                .filter(post -> post.getUserId() == id)  // `getUserId()` returns `int`, so `==` is fine
+                .toList();  // Convert the stream to a list
+    }
+
+
+
+    @PostMapping("/users/{id}/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post create(@PathVariable Integer id,
+                                       @RequestBody Post postRequest) {
+        var post = new Post();
+        post.setUserId(id);
+        post.setTitle(postRequest.getTitle());
+        post.setSlug(postRequest.getSlug());
+        post.setBody(postRequest.getBody());
+        posts.add(post);
+
+        return post;
+    }
+}
+// END
